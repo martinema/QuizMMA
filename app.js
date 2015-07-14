0750e5da6d69ab1cefe996ce509734f3211dfb83
Miguel Ángel Martínez <martinema@yahoo.com>
2015-07-14 20:17:13 +0200

    correccion
    corrección

--- app.js
+++ app.js
@@ -43,15 +43,17 @@ app.use(function(req, res, next){
 }
 
 //gestión de errores durante el desarrollo
-if(app.get('env') === 'development'){
-	app.use(function(err, req, res, next){
-		res.status(err.status || 500);
-		res.render('error', {
-			message: err.message, 
-			error: err; //Muestra err
-		});
-	});
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
 }
+
+
 
 //gestión de errores en producción
 app.use(function (err, req, res, next) {
