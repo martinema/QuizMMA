524354be67127647809633554c548e3e74dca187
Miguel Ángel Martínez <martinema@yahoo.com>
2015-07-14 20:50:59 +0200

    dd
    dd

--- models/quiz.js
+++
@@ -1,8 +0,0 @@
-// Definicion del modelo de Quiz
-
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('Quiz',
-            { pregunta: DataTypes.STRING,
-              respuesta: DataTypes.STRING,
-            });
-}
